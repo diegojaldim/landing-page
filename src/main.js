5006cc9fa7535788608417bb3287ef87a1808529
Diego <diego.jaldim@gmail.com>
2020-12-01 12:55:50 -0300

    Request without Vue

--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
-import Vue from 'vue';
-import App from './vue/home/App.vue';
+import constants from './helpers/constants';
+import { get } from './helpers/http';
 
-new Vue({
-    el: '#vue-job',
-    template: '<App />',
-    components: { App }
-});
+const url = `${constants.URL}${constants.endpoints.JOBS}`;
+
+const getJobs = url =>
+    get(url)
+        .then(res => res.data.vagas)
+        .then(jobs => jobs.filter(job => job.ativa));
+
+const list = document.querySelector('#list');
+
+let li = '';
+
+const renderJobs = job => {
+    let address = job.localizacao ? `${job.localizacao.bairro} - ${job.localizacao.cidade}, ${job.localizacao.pais}` : 'Remoto';
+    li += '<li><p class="job-title"><a href="http://elo7.dev/vaga/desenvolvedor-mobile-senior">'+ job.cargo +'</a></p><p class="job-locale">' + address +'</p></li>';
+};
+
+(async () => {
+    const jobs = await getJobs(url);
+    jobs.map(renderJobs);
+    list.innerHTML = li;
+})();
